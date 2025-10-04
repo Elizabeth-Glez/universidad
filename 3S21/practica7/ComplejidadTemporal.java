b8d69babaf503246d8a6d39b1c5a53d6c8c92d0f
Elizabeth Glez <btspoderosos@gmail.com>
2025-10-03 21:55:14 -0600

    Add files via upload

---
+++ 3S21/practica7/ComplejidadTemporal.java
@@ -0,0 +1,76 @@
+package practica7;
+
+public class ComplejidadTemporal {
+
+    public static int accesoConstante(int[] array) {
+        return array[0];
+    }
+
+    public static int sumaLineal(int[] array) {
+        int suma = 0;
+        for (int i = 0; i < array.length; i++) {
+            suma += array[i];
+        }
+        return suma;
+    }
+
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static int busquedaBinaria(int[] array, int objetivo) {
+        int izquierda = 0;
+        int derecha = array.length - 1;
+
+        while (izquierda <= derecha) {
+            int medio = izquierda + (derecha - izquierda) / 2;
+
+            if (array[medio] == objetivo) {
+                return medio;
+            }
+            if (array[medio] < objetivo) {
+                izquierda = medio + 1;
+            } else {
+                derecha = medio - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int fibonacciExponencial(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacciExponencial(n - 1) + fibonacciExponencial(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        System.out.println("Acceso constante: " + accesoConstante(array));
+        System.out.println("Suma lineal: " + sumaLineal(array));
+
+        int[] arrayParaOrdenar = {5, 3, 2, 4, 1};
+        bubbleSort(arrayParaOrdenar);
+        System.out.print("Bubble Sort: ");
+        for (int num : arrayParaOrdenar) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        int indice = busquedaBinaria(array, 3);
+        System.out.println("Búsqueda binaria (índice de 3): " + indice);
+
+        System.out.println("Fibonacci exponencial (n=5): " + fibonacciExponencial(5));
+    }
+}
